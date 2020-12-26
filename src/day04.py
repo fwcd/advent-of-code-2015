474a1fedbb6ef7629c704661482a31f73214b565
fwcd <fwcdmail@gmail.com>
2020-12-26 02:55:11 +0100

    Add day 4 part 2

--- src/day04.py
+++ src/day04.py
@@ -1,10 +1,12 @@
 import hashlib
 
-def mine(prefix):
+def mine(prefix, n):
     i = 0
-    while not hashlib.md5(f'{prefix}{i}'.encode()).hexdigest().startswith('0' * 5):
+    while not hashlib.md5(f'{prefix}{i}'.encode()).hexdigest().startswith('0' * n):
         i += 1
     return i
 
 with open('resources/day04.txt', 'r') as f:
-    print(f'Part 1: {mine(f.read().strip())}')
+    raw = f.read().strip()
+    print(f'Part 1: {mine(raw, 5)}')
+    print(f'Part 2: {mine(raw, 6)}')
