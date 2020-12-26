b63a626bc91113f3184a89f16f4a7e9e4352e0a7
fwcd <fwcdmail@gmail.com>
2020-12-26 02:48:50 +0100

    Complete day 3 part 2

--- src/day03.py
+++ src/day03.py
@@ -1,12 +1,24 @@
 with open('resources/day03.txt', 'r') as f:
-    x = 0
-    y = 0
-    visited = set()
+    x, y, sx, sy, rx, ry = (0, 0, 0, 0, 0, 0)
+    pt1_visited = set()
+    pt2_visited = set()
     def deliver():
-        visited.add(f'{x},{y}')
-    for c in f.read():
+        pt1_visited.add(f'{x},{y}')
+        pt2_visited.add(f'{sx},{sy}')
+        pt2_visited.add(f'{rx},{ry}')
+    for (i, c) in enumerate(f.read()):
         deliver()
-        x += 1 if c == '>' else -1 if c == '<' else 0
-        y += 1 if c == 'v' else -1 if c == '^' else 0
+        dx, dy = (0, 0)
+        dx = 1 if c == '>' else -1 if c == '<' else 0
+        dy = 1 if c == 'v' else -1 if c == '^' else 0
+        x += dx
+        y += dy
+        if i % 2 == 0:
+            sx += dx
+            sy += dy
+        else:
+            rx += dx
+            ry += dy
     deliver()
-    print(f'Part 1: {len(visited)}')
+    print(f'Part 1: {len(pt1_visited)}')
+    print(f'Part 2: {len(pt2_visited)}')
