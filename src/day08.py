4004b1d03ac73373ab6802114059c5a6d485628b
fwcd <fwcdmail@gmail.com>
2020-12-26 04:15:59 +0100

    Add day 8 part 2

--- src/day08.py
+++ src/day08.py
@@ -1,7 +1,10 @@
 with open('resources/day08.txt', 'r') as f:
     part1 = 0
+    part2 = 0
     for line in f.readlines():
         l = line.strip()
         if l:
             part1 += len(l) - len(eval(l))
+            part2 += len(l.__repr__().replace('"', '\\"')) - len(l)
     print(f'Part 1: {part1}')
+    print(f'Part 2: {part2}')
