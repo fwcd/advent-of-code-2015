8e1f76df71d6aeadde7d14364c9b6a37736db069
fwcd <fwcdmail@gmail.com>
2020-12-26 04:01:47 +0100

    Add day 7 part 2

--- src/day07.py
+++ src/day07.py
@@ -1,7 +1,7 @@
 import functools
 import re
 
-with open('resources/day07.txt', 'r') as f:
+def parse_circuit(raw):
     circuit = dict()
 
     @functools.lru_cache
@@ -11,8 +11,7 @@ with open('resources/day07.txt', 'r') as f:
         except:
             return circuit[x]()
 
-    for x, op, y, dest in re.findall(r'([0-9a-z]+)?\s*([A-Z]+)?\s*(\S+)?\s+->\s+(\w+)\n', f.read()):
-        print(f'x: {x}, op: {op}, y: {y}, dest: {dest}')
+    for x, op, y, dest in re.findall(r'([0-9a-z]+)?\s*([A-Z]+)?\s*(\S+)?\s+->\s+(\w+)\n', raw):
         if op == 'AND':
             circuit[dest] = lambda x=x, y=y: wire(x) & wire(y)
         elif op == 'OR':
@@ -25,5 +24,16 @@ with open('resources/day07.txt', 'r') as f:
             circuit[dest] = lambda y=y: ~wire(y)
         else:
             circuit[dest] = lambda x=x: wire(x)
+    
+    return circuit
 
-    print(f"Part 1: {circuit['a']()}")
+with open('resources/day07.txt', 'r') as f:
+    raw = f.read()
+    circuit1 = parse_circuit(raw)
+    part1 = circuit1['a']()
+    print(f"Part 1: {part1}")
+
+    circuit2 = parse_circuit(raw)
+    circuit2['b'] = lambda: part1
+    part2 = circuit2['a']()
+    print(f"Part 2: {part2}")
