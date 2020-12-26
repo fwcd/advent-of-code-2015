1ef4cb79b3ec27d9ef0368cf135182495fca2cd3
fwcd <fwcdmail@gmail.com>
2020-12-26 18:33:38 +0100

    Solve day 12 part2

--- src/day12.py
+++ src/day12.py
@@ -1,11 +1,22 @@
 import json
 
-def total(elem):
+def part2_total(elem):
     t = type(elem)
     if t is dict:
-        return sum(total(v) for _, v in elem.items())
+        return sum(part2_total(v) for k, v in elem.items()) if 'red' not in elem.values() else 0
     elif t is list:
-        return sum(map(total, elem))
+        return sum(map(part2_total, elem))
+    elif t is int:
+        return elem
+    else:
+        return 0
+
+def part1_total(elem):
+    t = type(elem)
+    if t is dict:
+        return sum(part1_total(v) for _, v in elem.items())
+    elif t is list:
+        return sum(map(part1_total, elem))
     elif t is int:
         return elem
     else:
@@ -14,7 +25,8 @@ def total(elem):
 def main():
     with open('resources/day12.json', 'r') as f:
         elem = json.loads(f.read().strip())
-        print(f'Part 1: {total(elem)}')
+        print(f'Part 1: {part1_total(elem)}')
+        print(f'Part 2: {part2_total(elem)}')
 
 if __name__ == "__main__":
     main()
