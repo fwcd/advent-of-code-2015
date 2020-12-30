11a2764ab8115267363b94082e015dcf5bf19aba
fwcd <fwcdmail@gmail.com>
2020-12-30 02:06:50 +0100

    Solve day 18 part 2

--- src/day18.py
+++ src/day18.py
@@ -1,21 +1,41 @@
-def step(grid):
+def is_corner(x, y, width, height):
+    return (x, y) in [(0, 0), (0, height - 1), (width - 1, 0), (width - 1, height - 1)]
+
+def step(grid, part2=False):
     new = [[cell for cell in row] for row in grid]
-    for y in range(len(grid)):
-        for x in range(len(grid[y])):
-            nbs = [grid[y + dy][x + dx] for dy in range(-1, 2) for dx in range(-1, 2) if (dy != 0 or dx != 0) and y + dy >= 0 and y + dy < len(grid) and x + dx >= 0 and x + dx < len(grid[y + dy])]
-            ons = len([nb for nb in nbs if nb == '#'])
-            if grid[y][x] == '#':
-                new[y][x] = '#' if ons in [2, 3] else '.'
-            else:
-                new[y][x] = '#' if ons == 3 else '.'
+    height = len(grid)
+    width = len(grid[0])
+    for y in range(height):
+        for x in range(width):
+            nbs = [
+                grid[y + dy][x + dx] == '#' or (part2 and is_corner(x + dx, y + dy, width, height))
+                for dy in range(-1, 2)
+                for dx in range(-1, 2)
+                if (dy != 0 or dx != 0) and y + dy >= 0
+                                        and y + dy < len(grid)
+                                        and x + dx >= 0
+                                        and x + dx < len(grid[y + dy])
+            ]
+            ons = len([nb for nb in nbs if nb])
+            on = grid[y][x] == '#'
+            new_on = (on and ons in [2, 3]) or (not on and ons == 3) or (part2 and is_corner(x, y, width, height))
+            new[y][x] = '#' if new_on else '.'
     return new
+
+def simulate(grid, n=100, part2=False):
+    new = grid
+    for i in range(n):
+        new = step(new, part2)
+    return new
+
+def total_ons(grid):
+    return len([cell for row in grid for cell in row if cell == '#'])
 
 def main():
     with open('resources/day18.txt', 'r') as f:
         grid = [l.strip() for l in f.readlines() if l.strip()]
-        for i in range(100):
-            grid = step(grid)
-        print(f"Part 1: {len([cell for row in grid for cell in row if cell == '#'])}")
+        print(f"Part 1: {total_ons(simulate(grid, part2=False))}")
+        print(f"Part 2: {total_ons(simulate(grid, part2=True))}")
 
 if __name__ == "__main__":
     main()
