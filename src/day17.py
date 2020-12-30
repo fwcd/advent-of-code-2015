85d1da6365674eb925ba30cd51c3087878e575c2
fwcd <fwcdmail@gmail.com>
2020-12-30 01:40:21 +0100

    Complete day 17 part 2

--- src/day17.py
+++ src/day17.py
@@ -11,8 +11,13 @@ def combinations(remaining):
 
 def main():
     with open('resources/day17.txt', 'r') as f:
+        goal = 150
         ns = [int(l.strip()) for l in f.readlines() if l.strip()]
-        print(f'Part 1: {len([c for c in combinations(ns) if sum(c) == 150])}')
+        cs = combinations(ns)
+        print(f'Part 1: {len([c for c in cs if sum(c) == goal])}')
+
+        mn = min(len(c) for c in cs if sum(c) == goal)
+        print(f'Part 2: {len([c for c in cs if sum(c) == goal and len(c) == mn])}')
 
 if __name__ == "__main__":
     main()
